7b280882667556ef234e89bb97a1859884f40fb9
Sneha Kumari <sneha.x.kumari.-nd@disney.com>
2020-04-05 13:03:48 +0530

    Added udemy

--- plugin/background.js
+++ plugin/background.js
@@ -2,7 +2,7 @@ chrome.runtime.onInstalled.addListener(function() {
     chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
         chrome.declarativeContent.onPageChanged.addRules([{
             conditions: [new chrome.declarativeContent.PageStateMatcher({
-                pageUrl: {urlMatches: '(youtube|hotstar|netflix|primevideo|facebook)\.com'},
+                pageUrl: {urlMatches: '(youtube|hotstar|netflix|primevideo|facebook|udemy)\.com'},
                 css: ["video"]
             })
             ],
